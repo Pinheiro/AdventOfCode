bf749b8c7fc7045d1a9c649cd2c8bd59a410a5a3
André Pinheiro <432938+Pinheiro@users.noreply.github.com>
2022-12-19 20:38:23 +1300

    day 12 part 2

--- Python/2022/2022-12-2.py
+++ Python/2022/2022-12-2.py
@@ -23,34 +23,40 @@ def InRange(position):
 def AtMostOneHigher(p2, p1):
     return (heights[p2.row][p2.col] <= heights[p1.row][p1.col] + 1)
 
+def FewestSteps():
+    queue = deque()
+    queue.append(start_position)
+    visited = set()
+    visited.add(tuple([start_position.row, start_position.col]))
+    while queue:
+        current_position = queue.popleft()
+        if current_position == destination:
+            return current_position.step
+        for dir in directions:
+            new_position = Position(current_position.row + dir[0], current_position.col + dir[1], current_position.step + 1)
+            if InRange(new_position):
+                if AtMostOneHigher(new_position, current_position):
+                    new_coordinates = tuple([new_position.row, new_position.col])
+                    if not (new_coordinates in visited):
+                        queue.append(new_position)
+                        visited.add(new_coordinates)
+
 fileName = 'E:/AdventOfCode/_Input/2022/2022-12.txt'
 heights = [[elevation for elevation in line.strip()] for line in open(fileName, 'r').read().splitlines()]
-current_position = Position(*Find('S'))
+start_position = Position(*Find('S'))
 destination = Position(*Find('E'))
-queue = deque()
-queue.append(current_position)
 directions = [[-1, 0], [1, 0], [0, -1], [0, 1]] # left, right, up, down
 maxRow, maxCol = len(heights), len(heights[0])
-heights[current_position.row][current_position.col] = 'a'
+heights[start_position.row][start_position.col] = 'a'
 heights[destination.row][destination.col] = 'z'
+start_positions = []
+for row in range(len(heights)):
+    for col in range(len(heights[row])):
+        if heights[row][col] == 'a':
+            start_positions.append(Position(row, col))
 heights = [[ord(height)-ord('a') for height in row] for row in heights]
-visited = set()
-visited.add(tuple([current_position.row, current_position.col]))
-while queue:
-    current_position = queue.popleft()
-    print(current_position)
-    if current_position == destination:
-        print(current_position.step)
-        break 
-    for dir in directions:
-        new_position = Position(current_position.row + dir[0], current_position.col + dir[1], current_position.step + 1)
-        print('-> ', new_position, InRange(new_position), end = ' ')
-        if InRange(new_position):
-            print(AtMostOneHigher(new_position, current_position), end = ' ')
-            if AtMostOneHigher(new_position, current_position):
-                new_coordinates = tuple([new_position.row, new_position.col])
-                print(not (new_coordinates in visited), end = ' ')
-                if not (new_coordinates in visited):
-                    queue.append(new_position)
-                    visited.add(new_coordinates)
-        print()
+steps = []
+for start_position in start_positions:
+    s = FewestSteps()
+    if s != None: steps.append(s)
+print(min(steps))
